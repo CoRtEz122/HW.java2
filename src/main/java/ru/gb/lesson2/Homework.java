4bcec6cbb4d0a35c608404bbabdd64cc5244d797
delfo <delformatik@gmail.com>
2023-06-10 19:15:39 +0300

    HW

--- src/main/java/ru/gb/lesson2/Homework.java
+++ src/main/java/ru/gb/lesson2/Homework.java
@@ -2,11 +2,18 @@ package ru.gb.lesson2;
 
 public class Homework {
 
+    public static void main(String[] args) {
+
+        String string = "abcdedcba";
+        System.out.println(isPalindrome(string));
+
+    }
+
     /**
      * 1. Реализовать метод isPalindrome
      * 2. Пересмотреть мой спич про работу с файловой системой. Подготовить вопросы.
      */
-    
+
     /**
      * Проверить, является ли строка палиндромом.
      * Палиндром - это строка, которая читается одинаково слева и справа
@@ -16,9 +23,33 @@ public class Homework {
      * abcd -> false
      * abba -> true
      */
-    static boolean isPalindrome(String source) {
+    static boolean isPalindrome(String string) {
         // TODO: 08.06.2023
-        return false;
+
+        char[] result = string.toCharArray();
+
+        String left = "";
+        String right = "";
+
+        for (int i = 0; i < result.length / 2; i++) {
+
+            left += result[i];
+            right += result[result.length - i - 1];
+        }
+        System.out.println("left = " + left);
+        System.out.println("right = " + right);
+
+        if (left.equals(right)) {
+
+            return true;
+
+        }
+
+        else {
+
+            return false;
+
+        }
     }
-    
+
 }
